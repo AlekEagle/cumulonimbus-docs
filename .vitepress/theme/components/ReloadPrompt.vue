a759a3c89d45337a57b61cb5d1aaa006903ee4fc
Alek Evans <github@alekeagle.com>
2023-12-13 10:48:48 -0900

    asdf

--- .vitepress/theme/components/ReloadPrompt.vue
+++ .vitepress/theme/components/ReloadPrompt.vue
@@ -63,18 +63,18 @@
     bottom: 0;
     margin: 16px;
     padding: 12px;
-    border: 1px solid #8885;
+    border: 1px solid var(--vp-c-border);
     border-radius: 4px;
     z-index: 100;
     text-align: left;
-    box-shadow: 3px 4px 5px 0 #8885;
-    background-color: white;
+    box-shadow: 3px 4px 5px 0 var(--vp-c-border);
+    background-color: var(--vp-c-bg);
   }
   .pwa-toast #pwa-message {
     margin-bottom: 8px;
   }
   .pwa-toast button {
-    border: 1px solid #8885;
+    border: 1px solid var(--vp-button-brand-bg);
     outline: none;
     margin-right: 5px;
     border-radius: 2px;
